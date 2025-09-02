5d131cd1e3745ca29e524d1fa765a5605614fd09
Aleksandr Zinin <fontercity@gmail.com>
2025-09-02 13:22:42 +0400

    refactor: update type import in regex check route and format sitemap XML

--- src/routes/check/[regex]/+page.server.ts
+++ src/routes/check/[regex]/+page.server.ts
@@ -1,7 +1,7 @@
-import type { PageLoadData } from './$types';
+import type { PageProps } from './$types';
 import { regexpChecker } from '$lib/regexpChecker';
 
-export async function load(data: PageLoadData) {
+export async function load(data: PageProps) {
   const rawRegex = data.params.regex;
   return { rawRegex, result: regexpChecker(rawRegex, true) };
 }
--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -5,24 +5,24 @@ export const GET: RequestHandler = async () => {
   const currentDate = new Date().toISOString().split('T')[0];
 
   const sitemap = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url>
-        <loc>${baseUrl}/</loc>
-        <lastmod>${currentDate}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>1.0</priority>
-      </url>
-      <url>
-        <loc>${baseUrl}/check</loc>
-        <lastmod>${currentDate}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.9</priority>
-      </url>
-    </urlset>
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${baseUrl}/</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>${baseUrl}/check</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+</urlset>
   `;
 
-  return new Response(sitemap, {
+  return new Response(sitemap.trim(), {
     headers: {
       'Content-Type': 'application/xml',
       'Cache-Control': 'max-age=3600', // Cache for 1 hour
